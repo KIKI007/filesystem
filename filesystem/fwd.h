90fb57f9698bce2961d3a23bc6c957abddaeff68
Ziqi Wang <qiqiustc@gmail.com>
2020-04-07 11:23:38 +0200

    Update fwd.h

--- filesystem/fwd.h
+++ filesystem/fwd.h
@@ -10,7 +10,7 @@
 #pragma once
 
 #if !defined(NAMESPACE_BEGIN)
-#define NAMESPACE_BEGIN(name) namespace name {
+#define NAMESPACE_BEGIN(name) namespace Wenzel::name {
 #endif
 #if !defined(NAMESPACE_END)
 #define NAMESPACE_END(name) }
